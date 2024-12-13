6843106b138b9d66af1d654ac40ad5c4dcba2767
ngounthengos <ngounthengos.lim@gmail.com>
2024-12-13 21:40:52 +0700

    nooo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3dunssve5og"
+    "revision": "0.l3mn0dl8om8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
